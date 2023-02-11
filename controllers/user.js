4b0c44c92b9f208de65b8855ee79d9d41aa4791c
dh5harris <dh5harris@hotmail.com>
2023-02-10 21:41:25 -0700

    added and fixed user controller

--- controllers/user.js
+++ controllers/user.js
@@ -1,6 +1,6 @@
 const mongodb = require('../db/connect');
-const ObjectId = require('mongodb').ObjectId;
 
+// Get all users
 const getAll = async(req, res) => {
   const result = await mongodb.getDb().db('portfolioproject').collection('users').find();
   result.toArray().then((lists) => {
@@ -9,16 +9,17 @@ const getAll = async(req, res) => {
   });
 };
 
+// Get a single user
 const getUser = async(req, res) => {
-  const userId = new ObjectId(req.params.id);
-	const user = {username: req.body.username};
-  const result = await mongodb.getDb().db('portfolioproject').collection('users').find({_id: userId},user);
+	const username = {username: req.params.username};
+	const result = await mongodb.getDb().db('portfolioproject').collection('users').find(username);
   result.toArray().then((lists) => {
     res.setHeader('Content-Type', 'application/json');
     res.status(200).json(lists[0]);
   }) 
 }
 
+// Create a user
 const createUser = async(req, res) => {
   const user = {
     username: req.body.username,
@@ -38,8 +39,9 @@ const createUser = async(req, res) => {
   }
 }
 
+// Update a user
 const updateUser = async(req, res) => {
-	const userId = new ObjectId(req.params.id);
+	const username = {username: req.params.username};
   const user = {
     username: req.body.username,
     password: req.body.password,
@@ -50,18 +52,19 @@ const updateUser = async(req, res) => {
 		skills: req.body.skills,
 		projects: req.body.projects
   };
-  const response = await mongodb.getDb().db('portfolioproject').collection('users').replaceOne({_id: userId}, user);
+  const response = await mongodb.getDb().db('portfolioproject').collection('users').replaceOne(username, user);
   console.log(response);
   if (response.modifiedCount > 0) {
     res.status(204).send();
   } else {
-    res.status(500).json(response.error || 'An error occured while updating the users.');
+    res.status(500).json(response.error || 'An error occured while updating the user.');
   }
 }
 
+// Delete a user
 const deleteUser = async(req, res) => {
-  const userId = new ObjectId(req.params.id);
-  const response = await mongodb.getDb().db('portfolioproject').collection('users').deleteOne({_id: userId}, true);
+  const username = {username: req.params.username};
+  const response = await mongodb.getDb().db('portfolioproject').collection('users').deleteOne(username);
   console.log(response);
   if (response.deletedCount > 0) {
     res.status(200).send();
